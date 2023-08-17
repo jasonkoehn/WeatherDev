903bafdfb22afde8c79e9bad1e2cb139ff89bbc7
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2023-08-16 19:33:45 -0700

    Initial Commit

---
+++ WeatherDev/WeatherDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDevApp.swift
+//  WeatherDev
+//
+//  Created by Jason Koehn on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
